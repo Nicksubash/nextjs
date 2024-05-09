ee04e8881cbdaf36f5c03eda56a1a16d93e0dd2f
スバシュ <parajuli.egrowth@gmail.com>
2024-05-09 14:06:03 +0900

    Committing changes to page.tsx before switching to main

--- src/app/Pages/Contact/page.tsx
+++ src/app/Pages/Contact/page.tsx
@@ -1,14 +1,14 @@
 import Navbar from "@/app/components/navbar";
 // import Whychoose from "../../components/whychoose";
 import ImageText from "@/app/components/ui/imageText";
+import Whychoose from "@/app/components/whychoose";
 
 export default function Bar(){
     return(
 <>
-
+<h1>This is contact page</h1>
         <Navbar/>
-       
-        <ImageText/>
+        <Whychoose/>
     
 </>
     )
